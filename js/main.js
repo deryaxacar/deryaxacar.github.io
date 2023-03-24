a9df1252bdfb6f2a489a18535e25590de58df58e
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2023-03-24 12:41:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-/* 
------------------
-https://github.com/ardacarofficial/links-website is open source project.
------------------
-*/
 
 
 $(document).ready(function(){
@@ -10,7 +5,6 @@ $(document).ready(function(){
         $("#menu-1").css("color", "var(--menu-active-text-color)");
         $("#menu-2").css("color", "var(--menu-text-color)");
         $("#menu-3").css("color", "var(--menu-text-color)");
-        $("#menu-4").css("color", "var(--menu-text-color)");
         $("#menu-1-container").show();
         $("#menu-2-container").hide();
         $("#menu-3-container").hide();
@@ -20,7 +14,6 @@ $(document).ready(function(){
         $("#menu-1").css("color", "var(--menu-text-color)");
         $("#menu-2").css("color", "var(--menu-active-text-color)");
         $("#menu-3").css("color", "var(--menu-text-color)");
-        $("#menu-4").css("color", "var(--menu-text-color)");
         $("#menu-1-container").hide();
         $("#menu-2-container").show();
         $("#menu-3-container").hide();
@@ -30,7 +23,6 @@ $(document).ready(function(){
         $("#menu-1").css("color", "var(--menu-text-color)");
         $("#menu-2").css("color", "var(--menu-text-color)");
         $("#menu-3").css("color", "var(--menu-active-text-color)");
-        $("#menu-4").css("color", "var(--menu-text-color)");
         $("#menu-1-container").hide();
         $("#menu-2-container").hide();
         $("#menu-3-container").show();
@@ -47,3 +39,14 @@ $(document).ready(function(){
         $("#menu-4-container").show();
     });
 });
+
+document.body.onload = () => {
+
+    $("#menu-1").css("color", "var(--menu-text-color)");
+    $("#menu-2").css("color", "var(--menu-active-text-color)");
+    $("#menu-3").css("color", "var(--menu-text-color)");
+    $("#menu-1-container").hide();
+    $("#menu-2-container").show();
+    $("#menu-3-container").hide();
+
+}
